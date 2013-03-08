0aca371ce9c380e8fbb5b1f5d7fba16de1ddc6e7
Gordon Brander <marlfoskr@gmail.com>
2013-03-08 16:07:12 -0700

    Kick off tick() immediately, not after timeout

--- fps-reduce.js
+++ fps-reduce.js
@@ -11,11 +11,13 @@ function fps(desiredFps) {
 
   return reducible(function reduceFps(next, result) {
     function tick() {
+      // Pass current time to `next()`, and accumulate result.
       result = next(Date.now(), result);
+      // If value has not been reduced, set a timer to go again.
       if(!isReduced(result)) setTimeout(tick, msPerFrame);
     }
 
-    setTimeout(tick, msPerFrame)
+    tick();
   })
 }
 
