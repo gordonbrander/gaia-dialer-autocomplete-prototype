bfc4b84f362e4cfb08038e747bcfbc06d82d55e0
Gordon Brander <marlfoskr@gmail.com>
2013-03-13 10:51:43 -0600

    Use setInterval/clearInterval instead of recursion

--- fps-reduce.js
+++ fps-reduce.js
@@ -10,15 +10,13 @@ function fps(desiredFps) {
   var msPerFrame = 1000 / desiredFps
 
   return reducible(function reduceFps(next, result) {
-    function tick() {
+    var intervalId = setInterval(function tick() {
       // Pass current time to `next()`, and accumulate result.
       result = next(Date.now(), result);
-      // If value has not been reduced, set a timer to go again.
-      if(!isReduced(result)) setTimeout(tick, msPerFrame);
-    }
-
-    tick();
-  })
+      // If value has been reduced, clear interval.
+      if(isReduced(result)) clearInterval(intervalId);
+    }, msPerFrame);
+  });
 }
 
 module.exports = fps;
